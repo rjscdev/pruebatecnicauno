e870067563bc6149657d59010a47ee99ad34feb5
Roberto <rjsc492004@gmail.com>
2024-07-25 08:26:53 -0600

    inicio prueba

---
+++ prueba tecnica 1.py
@@ -0,0 +1,13 @@
+#########
+#importar dependencias
+#########
+#########
+#Crear una funcion que muestre un mensaje si el dato de la entrada es igual a 5. si el dato no es 5 debe arrojar un mensaje de error
+#########
+#########
+#Crear interfaz en la cual se va a mostrar todo con una resolucion de 300x400
+#########
+#########
+#añadir elementos necesarios
+#########
+root.mainloop()
